f44ed638e7803667161a2026416f8ba2e464afdb
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-03-27 17:53:50 -0400

    Added user scores to Leaderboard page

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -6,6 +6,12 @@ module.exports = {
 
 async function show(req, res) {
     const users = await User.find({});
+
+    // sort the AI Platforms in ascending order by name
+    users.score.sort((a,b) => {
+        return (a < b) ? -1 : (a > b) ? 1 : 0;
+    });
+
     console.log(users)
     res.render("leaderboard", {
         title: "Leaderboard",
